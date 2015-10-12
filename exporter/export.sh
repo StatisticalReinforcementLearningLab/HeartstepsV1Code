bf220d1e46525b15b487db78cae3d9be77f9b114
aboruvka <audrey.boruvka@gmail.com>
2015-10-12 17:05:19 -0400

    More time fixes

--- exporter/export.sh
+++ exporter/export.sh
@@ -4,11 +4,14 @@
 export PATH=$PATH:/path/to/google_appengine/
 export GAE_SDK_ROOT=/Data/google_appengine
 
-dir=~/mbox/HeartSteps/HeartStepsTables
+dir=~/mbox/HeartSteps/Tables
+
+rm $dir/*.bak
 
 for table in EMA_Completed EMA_Context_Engaged EMA_Context_Notified EMA_Response Heartsteps_Usage_History Momentary_Decision Motivational_Message Response Snoozed_FromInApp Structured_Planning_Response Unstructured_Planning_Response User_Addresses User_Calendars User_Data User_Decision_Times User_Last_Updated Valid_Jawbone_Email_Addresses Valid_User_Email_Addresses Weather_History
 do
-echo "Exporting $table..."
+echo "EXPORTING $table..."
+mv $dir/$table.csv $dir/$table.bak
 ./appcfg.py download_data --url=https://com-um-heartsteps.appspot.com/remote_api --filename=$dir/$table.csv --kind=$table --config_file=config_files/$table.yaml
 done
 
