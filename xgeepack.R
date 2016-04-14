d5827196bda2a66d0b36925d3e30a0bd83db83af
nseewald1 <nseewald1@gmail.com>
2016-04-13 20:22:43 -0400

    Add matrix class to estimate() output

--- xgeepack.R
+++ xgeepack.R
@@ -322,7 +322,7 @@ estimate <- function(fit, combos = NULL, var = fit$var, ztest = TRUE,
   out <- cbind(est, lcl, ucl, se.est, pvalue)
   rownames(out) <- rownames(combos)
   colnames(out) <- c("Estimate", "95% LCL", "95% UCL", "SE", "p-value")
-  class(out) <- "estimate"
+  class(out) <- c("matrix", "estimate")
   out
 }
 
