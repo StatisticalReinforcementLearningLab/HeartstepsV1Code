c019906912d5dad61dce75af48166a225f5da599
wdempsey <dempsey.walter@gmail.com>
2017-03-27 15:45:58 -0400

    Added minute by minute step count plot creator

--- Walter/minute-level-step-plots.R
+++ Walter/minute-level-step-plots.R
@@ -14,24 +14,24 @@ plot.minute.steps <- function(id, day) {
   d <- subset(jawbone, user == id & start.date == as.Date(day.date))
   d$start.local.strip <- as.POSIXlt(d$start.utime.local)
   d$day.time <- d$start.local.strip$hour + d$start.local.strip$min / 60 + d$start.local.strip$sec / 60
-  with(d, plot(steps ~ day.time, type = "p", pch = 20, xlim = c(0, 24), ylim = c(-20, 210),
+  with(d, plot(steps ~ (day.time-5)%%24, type = "p", pch = 20, xlim = c(0, 24), ylim = c(-20, 210),
                ylab = "Minute-level step count", xlab = "Hours since midnight",
                main = paste0("Minute-by-minute step count\nUser ", id, ", Day ", day,
                              " (", day.date, ")")))
-  segments(d$day.time, 0, y1 = d$steps)
+  segments((d$day.time-5)%%24, 0, y1 = d$steps)
   
   dsuggest <- subset(suggest, user == id & study.date == as.Date(day.date))
   dsuggest$start.local.strip <- as.POSIXlt(dsuggest$utime.stamp)
   dsuggest$day.time <- dsuggest$start.local.strip$hour + dsuggest$start.local.strip$min / 60 + dsuggest$start.local.strip$sec / 60
-  temp = dsuggest$day.time[dsuggest$link==1 & dsuggest$avail == TRUE]
+  temp = (dsuggest$day.time[dsuggest$link==1 & dsuggest$avail == TRUE]-5)%%24
   points(temp, rep(-5,length(temp)), pch = 16, col = "red")
   segments(temp-0.5, rep(-5,length(temp)), temp+0.5)
   
-  temp.nolink = dsuggest$day.time[dsuggest$link==0 & dsuggest$avail == TRUE]
+  temp.nolink = (dsuggest$day.time[dsuggest$link==0 & dsuggest$avail == TRUE]-5)%%24
   points(temp.nolink, rep(-10,length(temp.nolink)), pch = 15, col = "red")
   segments(temp.nolink-0.5, rep(-10,length(temp.nolink)), temp.nolink+0.5)
   
-  temp.unavail = dsuggest$day.time[dsuggest$avail == FALSE]
+  temp.unavail = (dsuggest$day.time[dsuggest$avail == FALSE]-5)%%24
   points(temp.unavail, rep(-20,length(temp.unavail)), pch = 15, col = "blue")
   segments(temp.unavail-0.5, rep(-20,length(temp.unavail)), temp.unavail+0.5)
   
@@ -48,6 +48,10 @@ for (i in as.numeric(sample(rownames(daily[!is.na(daily$study.day.nogap) &
 }
 
 user = 11
-for (i in which(daily$user == user)) {
+obs = which(daily$user == user)
+for (i in obs[1:6]) {
+  mypath <- file.path("/Users/walterdempsey/Dropbox/Presentations/Latent Variables",paste("user11_day", daily$study.day.nogap[i], ".png", sep = ""))
+  png(file=mypath)
   plot.minute.steps(daily$user[i], daily$study.day.nogap[i])
+  dev.off()
 }
