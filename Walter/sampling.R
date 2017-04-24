8cefeb170cbb70d9b55d121a8f81cb31b9b1cf3b
wdempsey <dempsey.walter@gmail.com>
2017-04-24 14:48:52 -0400

    updated sampling ot be wrt most recent defn of sedentary

--- Walter/sampling.R
+++ Walter/sampling.R
@@ -1,3 +1,4 @@
+library(ggplot2); library(gridExtra)
 require(doParallel)
 
 # reads the list of nodes which have been allocated
@@ -12,6 +13,7 @@ registerDoParallel(cl)
 
 ## Required packages and source files
 source("functions.R");require(mgcv); require(lubridate); require(foreach)
+setwd("/Volumes/dav/HeartSteps/Walter/")
 window.time = read.csv("window_time.csv")
 Sedentary.values = read.csv("sed_values.csv")
 Sedentary.length = read.csv("sed_length.csv")
@@ -33,18 +35,17 @@ for (i in 1:length(seq.hour)) {
 fraction.df = data.frame(fraction.data)
 names(fraction.df) = c("current.hour", "mean", "var")
 
-# png("../fraction_sed.png", width = 6.5,height = 3, units = "in", res = 300)
-par(mfrow = c(1,1), mar = c(5,4,1,1)+0.1)
-plot((fraction.df$current.hour-14)%%24,fraction.df$mean,
-     xlim = c(0,12),
-     ylim = c(0.24, 0.27), axes = FALSE,
-     ylab = "Fraction of time 'Sedentary'",
-     xlab = "Hour (0 = 9AM) ")
-axis(side = 1); axis(side = 2)
-# dev.off()
+
+
+png("/Users/walterdempsey/Dropbox/MRTs/WD & SAM work/Draft for randomization prob paper/mucmd/figs/heartsteps/fraction_sed.png", width = 6.5,height = 3, units = "in", res = 300)
+ggplot(data=fraction.df, aes(x=(current.hour-14)%%24, y=mean)) +
+  geom_line()+
+  geom_point()+ scale_y_continuous(limits = c(0.45, 0.55)) +
+  xlab("Hour (0 = 9AM)") + ylab("Fraction of time 'Sedentary'")
+dev.off()
 
 ## Setup
-N = c(0.0,2.25)
+N = c(0.0,1.74)
 eta = 0.0
 lambda = 0.0
 
@@ -58,10 +59,11 @@ total.At = foreach(i=1:num.iters,  .combine='cbind') %do% random.assignment.fn(a
 mean(colSums(total.At[1:136,]), na.rm = TRUE)
 sd(colSums(total.At[1:136,]), na.rm = TRUE)/sqrt(num.iters)
 
-png("/Volumes/dav/HeartSteps/Walter/summary_plot_At.png", width = 6.5, height = 4, units = "in", res = 300)
-par(mfrow = c(1,2), mar = c(5,4,1,1)+0.1)
+# png("/Volumes/dav/HeartSteps/Walter/summary_plot_At.png", width = 6.5, height = 4, units = "in", res = 300)
+sim.df = data.frame(colSums(total.At[1:136,]))
+names(sim.df) = c("ints")
 
-hist(colSums(total.At[1:136,]), main = "", xlab = "# of interventions", cex.axis = 0.75, cex.lab = 0.75)
+p1 <- ggplot(data=sim.df, aes(ints)) + geom_histogram() + xlab("Number of interventions")
 
 time.steps = 1:nrow(total.At[1:136,])
 hour = floor(time.steps/12 + 14 + 35/60)%%24
@@ -79,31 +81,40 @@ num.At = hourly.results.At$num.At[order(altered.hour.At)]
 
 altered.hour.At = altered.hour.At[order(altered.hour.At)]
 
-plot(altered.hour.At,num.At, axes = FALSE,
-     ylab = "Fraction of time receiving EMI per Hour", xlab = "Hour",
-     pch = 18, ylim = c(0.005, 0.02),
-     xlim = c(0,12), cex.lab = 0.75)
-axis(side = 1, cex.axis = 0.75); axis(side = 2, cex.axis = 0.75)
+temp.df = data.frame(cbind(altered.hour.At, num.At))
 
-lw1 = loess(num.At~altered.hour.At)
+p2 <- ggplot(data=temp.df, aes(x=altered.hour.At, y=num.At)) +
+  geom_line()+
+  geom_point()+ scale_y_continuous(limits = c(0.005, 0.02)) +
+  xlab("Hour (0 = 9AM)") + ylab("Fraction of time receiving EMI per Hour")
 
-lines(altered.hour.At, lw1$fitted,col = "red", lty = 2)
-dev.off()
+# plot(altered.hour.At,num.At, axes = FALSE,
+#      ylab = "Fraction of time receiving EMI per Hour", xlab = "Hour",
+#      pch = 18, ylim = c(0.005, 0.02),
+#      xlim = c(0,12), cex.lab = 0.75)
+# axis(side = 1, cex.axis = 0.75); axis(side = 2, cex.axis = 0.75)
 
-png("/Volumes/dav/HeartSteps/Walter/summary_plot_Xt.png", width = 6.5,height = 4, units = "in", res = 300)
-altered.hour.Xt = (hourly.results.Xt$hour-14)%%24
-num.Xt = hourly.results.Xt$num.Xt[order(altered.hour.Xt)]
+# lw1 = loess(num.At~altered.hour.At)
+# 
+# lines(altered.hour.At, lw1$fitted,col = "red", lty = 2)
 
-altered.hour.Xt = altered.hour.Xt[order(altered.hour.Xt)]
+grid.arrange(p1,p2,ncol = 2)
+# dev.off()
 
-par(mfrow = c(1,1), mar = c(5,4,1,1)+0.1)
-plot(altered.hour.Xt,num.Xt, axes = FALSE, ylab = "Fraction of time classified as 'Sedentary' ", xlab = "Hour",
-     pch = 18, ylim = c(0.2,0.35), xlim = c(0,12))
-axis(side = 1); axis(side = 2)
-
-lw1 = loess(num.Xt~altered.hour.Xt)
-
-lines(altered.hour.Xt, lw1$fitted,col = "red", lty = 2)
-dev.off()
+# png("/Volumes/dav/HeartSteps/Walter/summary_plot_Xt.png", width = 6.5,height = 4, units = "in", res = 300)
+# altered.hour.Xt = (hourly.results.Xt$hour-14)%%24
+# num.Xt = hourly.results.Xt$num.Xt[order(altered.hour.Xt)]
+# 
+# altered.hour.Xt = altered.hour.Xt[order(altered.hour.Xt)]
+# 
+# par(mfrow = c(1,1), mar = c(5,4,1,1)+0.1)
+# plot(altered.hour.Xt,num.Xt, axes = FALSE, ylab = "Fraction of time classified as 'Sedentary' ", xlab = "Hour",
+#      pch = 18, ylim = c(0.4,0.6), xlim = c(0,12))
+# axis(side = 1); axis(side = 2)
+# 
+# lw1 = loess(num.Xt~altered.hour.Xt)
+# 
+# lines(altered.hour.Xt, lw1$fitted,col = "red", lty = 2)
+# dev.off()
 
 stopCluster(cl)
