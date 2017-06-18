6d323511531599b325948e9693eed004d2f15d29
Nick Seewald <nseewald@umich.edu>
2017-06-18 15:48:14 -0400

    Create appuse data.frame for session-level app usage

--- User Experience Analysis.R
+++ User Experience Analysis.R
@@ -36,6 +36,8 @@ ids     <- primary$ids
 primary <- primary$data
 
 ##### Descriptives #####
+
+# Participant demographics
 mean(users$age[users$user %in% ids])
 sum(users$age[users$user %in% ids] <= 25)
 table(users$gender[users$user %in% ids]) / length(ids)
@@ -43,19 +45,22 @@ table(users$ethnicity[users$user %in% ids]) / length(ids)
 table(users$education[users$user %in% ids]) / length(ids)
 table(users$marital[users$user %in% ids]) / length(ids)
 
+# Number of participants who've used a fitness tracker or fitness app previously
 table(users$fittracker[users$user %in% ids])
 table(users$fitapp[users$user %in% ids])
 
+# Summarize activity at the day level
 mean(users$vigact.days.intake[users$user %in% ids])
 sd(users$vigact.days.intake[users$user %in% ids])
 mean(users$modact.days.intake[users$user %in% ids])
 sd(users$modact.days.intake[users$user %in% ids])
 
+# Tabulate IPAQ activity levelsl
 sum(users$ipaq.minimal.intake[users$user %in% ids & !is.na(users$ipaq.minimal.intake)])
 sum(users$ipaq.hepa.intake[users$user %in% ids & !is.na(users$ipaq.hepa.intake)])
 with(subset(users, user %in% ids), table(ipaq.hepa.intake, ipaq.minimal.intake))
 
-
+# Number of participants using their own phones
 sum(users$own.phone[users$user %in% ids])
 
 # Find users' last days on study
@@ -85,6 +90,26 @@ daily2 <- merge(daily, users, by = "user", all = T)
 # Number of total EMAs delivered
 emacount <- aggregate(ema.set.length ~ user, data = subset(daily, user %in% ids & !is.na(study.day.nogap) & study.day.nogap <= 41), function(x) sum(!is.na(x)))
 
-##### Mixed Effects Modeling #####
-summary(aov(I(log(jbsteps.direct)) ~ ipaq.hepa.intake + ipaq.minimal.intake, data = subset(daily2, !is.na(study.day.nogap) & study.day.nogap == 1 & user %in% ids)))
+##### Modeling #####
+summary(lm(I(log(jbsteps.direct)) ~ ipaq.hepa.intake + ipaq.minimal.intake, data = subset(daily2, !is.na(study.day.nogap) & study.day.nogap == 1 & user %in% ids)))
+summary(lm(I(log(jbsteps.direct)) ~ selfeff.intake, data = subset(daily2, !is.na(study.day.nogap) & study.day.nogap == 1 & user %in% ids)))
 
+
+##### App Usage by Session #####
+# session must be at least 2 seconds long; screen visit must be 2 seconds as well
+appuse <- aggregate(start.time ~ user + session.index, data = usage, function(x) x[1])
+appuse <- merge(appuse, aggregate(end.time ~ user + session.index, data = usage, FUN = function(x) x[length(x)]), by = c('user', 'session.index'))
+appuse <- merge(appuse, aggregate(tz ~ user + session.index, data = usage, function(x) x[1]), by = c('user', 'session.index'))
+appuse <- merge(appuse, aggregate(gmtoff ~ user + session.index, data = usage, function(x) x[1]), by = c('user', 'session.index'))
+appuse <- subset(appuse, user %in% ids)
+appuse <- appuse[order(appuse$user, appuse$session.index), ]
+appuse <- cbind(appuse,
+                 "start.utime" = char2utime(appuse$start.time, offset = appuse$gmtoff),
+                 "end.utime" = char2utime(appuse$end.time, offset = appuse$gmtoff),
+                 char2calendar(appuse$start.time, tz = appuse$tz, prefix = "start"),
+                 char2calendar(appuse$end.time, tz = appuse$tz, prefix = "end"))
+appuse$duration <- with(appuse, difftime(end.utime, start.utime, units = "secs"))
+
+hist(as.numeric(appuse$duration)[as.numeric(appuse$duration) <= 20],
+     main = "Duration of 'Sessions' of HeartSteps App Usage",
+     xlab = "Duration (sec)")
